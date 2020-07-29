ec04925858f287b77c9f610aaf41e3195b50f45b
strikermcs <tellermcs@gmail.com>
2020-07-29 16:22:15 +0300

    add views and layouts

--- src/router/index.js
+++ src/router/index.js
@@ -8,8 +8,58 @@ Vue.use(VueRouter)
   {
     path: '/',
     name: 'Home',
+    meta: {layout: 'main'},
     component: Home
   },
+  {
+    path: '/login',
+    name: 'login',
+    meta: {layout: 'empty'},
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {layout: 'main'},
+    component: () => import('../views/Categories.vue')
+  },
+  {
+    path: '/detail',
+    name: 'detail-record',
+    meta: {layout: 'main'},
+    component: () => import('../views/Detail-record.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {layout: 'main'},
+    component: () => import('../views/History.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {layout: 'main'},
+    component: () => import('../views/Planning.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {layout: 'main'},
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {layout: 'main'},
+    component: () => import('../views/Record.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: {layout: 'empty'},
+    component: () => import('../views/Register.vue')
+  },
+  
   
 ]
 
