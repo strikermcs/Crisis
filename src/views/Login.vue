4cfc90271d7a579833d4b28d7a9914f8296e90c4
strikermcs <tellermcs@gmail.com>
2020-07-30 13:08:58 +0300

    add validations form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,26 +1,55 @@
 <template>
-    <form class="card auth-card">
-  <div class="card-content">
-    <span class="card-title">Домашняя бухгалтерия</span>
+    <form class="card auth-card" @submit.prevent="submitHandler">
+
+      <div class="card-content">
+    <span class="card-title">NCrisis v0.0.1</span>
+      <span>Домашняя бухгалтерия</span>
     <div class="input-field">
       <input
           id="email"
           type="text"
-          class="validate"
+          v-model.trim="email"
+          :class="{invalid: ($v.email.$dirty && !$v.email.required)} || ($v.email.$dirty && !$v.email.email)"
       >
       <label for="email">Email</label>
-      <small class="helper-text invalid">Email</small>
+      <small 
+      v-if="$v.email.$dirty && !$v.email.required"
+      class="helper-text invalid">Поле не должно быть пустым</small>
+      
+      
+      <small 
+      v-else-if="$v.email.$dirty && !$v.email.email"
+      class="helper-text invalid">Введите коректный email</small>
+
+      
     </div>
+
     <div class="input-field">
       <input
           id="password"
           type="password"
-          class="validate"
+          v-model.trim="password"
+          :class="{invalid: ($v.password.$dirty && !$v.password.required)} || ($v.password.$dirty && !$v.password.minLength)"
+
       >
       <label for="password">Пароль</label>
-      <small class="helper-text invalid">Password</small>
+      <small
+      v-if="$v.password.$dirty && !$v.password.required" 
+      class="helper-text invalid">Поле не должно быть пустым</small>
+
+       <small
+      v-else-if="$v.password.$dirty && !$v.password.minLength" 
+      class="helper-text invalid">Пароль должен содержать {{$v.password.$params.minLength.min}} или больше символов.
+       Сейчас он {{password.length}}</small>
+
     </div>
-  </div>
+    
+
+
+  </div>  
+
+
+
   <div class="card-action">
     <div>
       <button
@@ -34,8 +63,38 @@
 
     <p class="center">
       Нет аккаунта?
-      <a href="/">Зарегистрироваться</a>
+      <router-link to="/register">Зарегистрироваться</router-link>
     </p>
   </div>
 </form>
 </template>
+
+<script>
+import {email, required,  minLength} from 'vuelidate/lib/validators'
+
+export default {
+  name: 'login',
+  data: () => ({
+    email: '',
+    password: ''
+  }), 
+  validations:{
+    email: {email, required},
+    password: {required, minLength: minLength(6)}
+  },
+
+  methods:{
+    submitHandler(){
+      if(this.$v.$invalid){
+        this.$v.$touch()
+        return
+      }
+      const formData = {
+        email: this.email,
+        password: this.password
+      }
+      this.$router.push('/')
+    }
+  }
+}
+</script>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,36 +1,65 @@
 <template>
-    <form class="card auth-card">
+    <form class="card auth-card" @submit.prevent="submitHandler">
   <div class="card-content">
-    <span class="card-title">Домашняя бухгалтерия</span>
-    <div class="input-field">
+    <span class="card-title">NCrisis v0.0.1</span>
+      <span>Домашняя бухгалтерия</span>
+      <div class="input-field">
       <input
           id="email"
           type="text"
+          v-model.trim="email"
+          :class="{invalid: ($v.email.$dirty && !$v.email.required)} || ($v.email.$dirty && !$v.email.email)"
       >
       <label for="email">Email</label>
-      <small class="helper-text invalid">Email</small>
+      <small 
+      v-if="$v.email.$dirty && !$v.email.required"
+      class="helper-text invalid">Поле не должно быть пустым</small>
+      
+      
+      <small 
+      v-else-if="$v.email.$dirty && !$v.email.email"
+      class="helper-text invalid">Введите коректный email</small>
+
+      
     </div>
+
     <div class="input-field">
       <input
           id="password"
           type="password"
-          class="validate"
+          v-model.trim="password"
+          :class="{invalid: ($v.password.$dirty && !$v.password.required)} || ($v.password.$dirty && !$v.password.minLength)"
+
       >
       <label for="password">Пароль</label>
-      <small class="helper-text invalid">Password</small>
+      <small
+      v-if="$v.password.$dirty && !$v.password.required" 
+      class="helper-text invalid">Поле не должно быть пустым</small>
+
+       <small
+      v-else-if="$v.password.$dirty && !$v.password.minLength" 
+      class="helper-text invalid">Пароль должен содержать {{$v.password.$params.minLength.min}} или больше символов.
+       Сейчас он {{password.length}}</small>
+
     </div>
+
+
+
     <div class="input-field">
       <input
           id="name"
           type="text"
-          class="validate"
+          v-model.trim="name"
+          :class="{invalid: $v.name.$dirty && !$v.name.required}"
       >
       <label for="name">Имя</label>
-      <small class="helper-text invalid">Name</small>
+      <small 
+      v-if="$v.name.$dirty && !$v.name.required"
+      class="helper-text invalid">Введите ваше имя</small>
     </div>
     <p>
       <label>
-        <input type="checkbox" />
+        <input type="checkbox" v-model="agree"/>
         <span>С правилами согласен</span>
       </label>
     </p>
@@ -48,9 +77,49 @@
 
     <p class="center">
       Уже есть аккаунт?
-      <a href="/">Войти!</a>
+      <router-link to="/login">Войти!</router-link>
     </p>
   </div>
 </form>
 
 </template>
+
+
+<script>
+import {email, required,  minLength} from 'vuelidate/lib/validators'
+
+export default {
+  name: 'register',
+  data: () => ({
+    email: '',
+    password: '',
+    name: '',
+    agree: false
+  }), 
+  validations:{
+    email: {email, required},
+    password: {required, minLength: minLength(6)},
+    name: {required},
+    agree: {checked: v => v}
+  },
+  methods:{
+    submitHandler(){
+      if(this.$v.$invalid){
+        this.$v.$touch()
+        return
+      }
+
+      const formData = {
+        email: this.email,
+        password: this.password,
+        name: this.name
+      }
+
+      this.$router.push('/')
+
+      console.log(formData)
+
+    }
+  }
+}
+</script>
